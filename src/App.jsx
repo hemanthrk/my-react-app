bb33b227324f8b97fb75917714b5c90f7f0fd862
hemanthrk <hemanthrk@gmail.com>
2026-01-17 14:46:03 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,239 @@
+import { useState, useRef, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import footballers from "./footballers";
+import {useDebounce} from "./hooks/useDebounce";
+
+function App() {
+  const [count, setCount] = useState(0);
+  //console.log("before use effect");
+useEffect(() =>{
+  document.title = `The current count is ${count}`;
+  console.log("component renders, useEffect");
+},[count]);
+
+  const [user,setUser] = useState({name:"john Doe", age:31, city:"LA"});
+  const [items, setItems] = useState([{id:0, name:"Item 1"}, {id:1, name:"Item 2"}, {id:2, name:"Item 3"}]);
+  const [color, setColor] = useState("#FFFFFF");
+  const inputRef = useRef(null);
+
+
+    const [query, setQuery] = useState("");
+    const debouncedQuery = useDebounce(query,1000);
+    const[resultQ, setResultQ]  = useState("");
+  const [fQuery, setFQuery] = useState("");
+  const [fResults, setFResults] = useState([]);
+  const [otp, setOtp] = useState();
+  const [counter5, setCounter5] = useState(5);
+
+  function handleClick(){
+     const minn = 100000;
+    const maxx = 999999;
+    const newN = Math.floor( Math.random() *(maxx-minn +1) )+minn;
+    setOtp(newN);
+
+      const intervalId = setInterval(()=>{
+      setCounter5((prevCounter)=> prevCounter -1);
+      return ()=> clearInterval(intervalId);
+    },1000)
+  }
+
+  useEffect(()=>{
+    console.log("counter5 ",counter5)
+    if(counter5 === 0){
+      return;
+    }
+    
+
+  
+
+   },[])
+ 
+
+
+  function handleSubmit(e){
+    e.preventDefault()
+}
+    useEffect(() =>{
+      console.log("debouncedquery ",debouncedQuery);
+      if(debouncedQuery){
+        const results = footballers.filter((footballer) => footballer.toLowerCase().includes(debouncedQuery.toLowerCase()));
+        console.log("search results ",results, resultQ, results[0]);
+       setResultQ(results);
+        console.log("search results ", resultQ);
+      }else{
+        console.log("Search Results: []");
+      }
+    },[debouncedQuery] )
+
+    useEffect(()=>{
+      if(fQuery.trim() ===''){
+        setFResults([]);
+        return ;
+      }
+
+      const timeoutId = setTimeout(async()=>{
+        try{
+        const response = await fetch(`https://fruit-search.freecodecamp.rocks/api/fruits?q=${query}`);
+         if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    console.log("response to fruit query is ",response)
+        const data = await response.json();
+        console.log("fetch data ",data);
+ 
+        const dataf = data.filter((da)=>da.name.toLowerCase().includes(fQuery.toLowerCase()));
+        setFResults(dataf.map(fruit => fruit.name))
+        }catch(error){
+          console.error("Error fetching data ",error);
+        }
+      },700)
+
+
+    },[fQuery])
+ 
+  
+
+  const handleFocus = () =>{
+    if(inputRef.current){
+      console.log("inputRef.current ",inputRef.current)
+      inputRef.current.focus();
+    }
+  };
+
+  const handleColorChange = (event) =>{
+      console.log("color event ",event);
+      setColor(event.target.value);
+  };
+
+  const addItem = () =>{
+    const newItem = {id:items.length+1, name: `Item ${items.length +1}`};
+    console.log("newItem is ",newItem);
+    items.push(newItem);
+   // setItems(items);
+   setItems((prevItem)=>[ ...prevItem, newItem] );
+  }
+  const removeItem = (id) =>{
+    console.log("remove id ", id);
+    setItems((prevItem) => prevItem.filter((item) => item.id !== id) );
+  };
+
+  const handleChange = (e) =>{
+    console.log("e is ",e);
+    //user.age = e.target.value;
+    //console.log("user is ",user)
+    // setUser((prevUser) => {
+    //   const updatedUser = {...prevUser,age:e.target.value};
+    //   console.log("prev state ",prevUser);
+    //   console.log("updated state ",updatedUser);
+    //   return updatedUser;
+    // });
+    //updating object in react, crucial step
+    const {name,value} = e.target;
+    console.log("e.target is ",e.target);
+   // updating object in react
+    setUser((prevUser) => ({...prevUser,[name]:value} ))
+  };
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <div><h1>User profile</h1>
+      <p>Name: {user.name}</p>
+      <p>Age: {user.age}</p>
+      <p>City: {user.city }</p>
+      <h2>Update User Age</h2>
+      <input type="number" name="age" value={user.age} onChange={handleChange} />
+      <input type="text" name="name" value={user.name} onChange={handleChange} />
+      <input type="text" name="city" value={user.city} onChange={handleChange} />
+      </div>
+      <div>
+        <button onClick={addItem}>Add Item</button>
+        <ul>
+          {items.map((item) =>(
+            <li key={item.id}>{item.name} {" "} <button onClick={ ()=> removeItem(item.id) } >Remove</button> </li>
+          ))}
+        </ul>
+      </div>
+      
+        <div id= "color-change" style={{backgroundColor:color}}>
+          <input type='color' value={color} onChange={handleColorChange} />
+        </div>
+        <div>
+          <input ref={inputRef} type="text" placeholder='Enter text' />
+          <button onClick={handleFocus} >Focus Input</button>
+        </div>
+        <div>
+          <h1 style={{textAlign:"center"}} >Footballer Search App</h1>
+          <div style={{textAlign:"center"}} >
+            <input 
+            style={{padding:"0.5rem", width:"30%"}}
+            type='text'
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            placeholder='Search for a footballer...'
+            />
+           
+          </div>
+          <div style={{textAlign:"center"}} >
+            <input 
+            style={{padding:"0.5rem", width:"30%"}}
+            type='text'
+            value={resultQ}
+            onChange={(e) => setQuery(e.target.value)}
+            placeholder='Search for a footballer...'
+            />
+           
+          </div>
+        </div>
+
+        <div>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor='search-input'>Search for fruits:</label>
+            <input 
+            id="search-input"
+            type='search'
+            value={fQuery}
+            onChange={(e) => setFQuery(e.target.value)}
+            />
+          </form>
+          <div>
+            {fResults.length > 0 ? fResults.map( item => <p key={item} >{item}</p>): <p>No Results found</p> }
+          </div>
+        </div>
+        <div>
+          <h1>OTP Generator</h1>
+          <h2>{ otp? otp: "Click 'Generate OTP' to get a code"}</h2>
+          
+          <p aria-live='polite'>
+            { counter5 > 0 ? `Expires in: ${counter5} seconds`: "OTP expired. Click the button to generate a new OTP." }
+          </p>
+          <button onClick={handleClick} disabled={ (counter5 >0 && counter5 <5) ? true:false} >Generate OTP</button>
+        </div>
+      
+    </>
+  );
+
+}
+
+export default App
